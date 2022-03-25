ae8f3b251e950880b543a5a13f9e6df436afdbab
Khaled-Waled <khaledw.hamed001@gmail.com>
2022-03-25 14:00:25 +0200

    Added the ability to change the popup color

--- popup.js
+++ popup.js
@@ -1,5 +1,30 @@
-let changeColor = document.getElementById("changeColor");
+var popupColorPicker;
+var defaultColor = "#0000ff";
+window.addEventListener("load", startup, false);
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+
+function startup() {
+    popupColorPicker = document.querySelector("#popupColorPicker");
+    popupColorPicker.value = defaultColor;
+    popupColorPicker.addEventListener("input", update_popup_BG_Color, false);
+    popupColorPicker.select();
+  }
+
+  
+function update_popup_BG_Color(event) {
+    document.body.style.backgroundColor = event.target.value;
+ };
+  
+  
+/*
+colorPicker.addEventListener("input", updateFirst, false);
+    console.log("AAAAAAAAAAA");
+
 });
+
+function colorChanged()
+{
+    console.log("AAAAAAAA");
+    document.body.style.backgroundColor = "#00000";
+}
+*/
